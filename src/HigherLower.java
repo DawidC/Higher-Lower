2a977615a22aa9b7259f3277cc75723d90e79329
DawidC <dawid.code+github@gmail.com>
2015-07-26 20:13:22 +0200

    zakończenie prac na dzień 26.07, początek randomu

--- src/HigherLower.java
+++ src/HigherLower.java
@@ -24,13 +24,45 @@ public class HigherLower {
                 System.out.print("Wybierz wersję: 1:Łatwiejsza(1-10), 2:Trudniejsza(1-100). : ");
                 int menu2 = sc.nextInt();
                 int iloscBledow = 0;
+                int liczba = 0;
+                int wynik = 0;
+                int liczbaZyc = 0;
+                Random generator = new Random();
                 switch(menu2){
+
                     case 1:
                         System.out.println("Będziemy wybierać liczby od 1 do 10, system wylosuje liczbę a " +
                                 "Twoim zadaniem będzie wybrać czy następna liczba będzie większa\n" +
                                 "czy mniejsza, za poprawny wybór otrzymasz + 10pkt, jeżeli odpowiesz" +
                                 " 3 razy źle - przegrywasz. Powodzenia! :)");
                                 iloscBledow = 3;
+
+                                while(iloscBledow>liczbaZyc){
+                                    int liczbaR = generator.nextInt(10);
+                                    liczbaR++;
+                                    for(int i = 0;i<50;i++){
+                                        System.out.println();
+                                    }
+                                    System.out.print("Szanse: ");
+                                    for(int i = 0;i<iloscBledow;i++){
+                                        System.out.print("X ");
+                                    }
+                                    System.out.println();
+                                    System.out.print("Liczba: ");
+                                    System.out.print(liczbaR);
+                                    System.out.print(". Następna liczba będzie: 1.Mniejsza, 2.Większa. : ");
+                                    int ltmp = sc.nextInt();
+                                    if(ltmp == 1){
+
+                                    } else if(ltmp == 2){
+
+                                    } else {
+                                        System.out.println("ZŁY Wybór!!!");
+                                        break;
+                                    }
+
+
+                                }
 
                         break;
 
